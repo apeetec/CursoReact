09f0b9a5b67c40fbf80f56fc05a638086ad06530
Gabriel <gabriel938@gmail.com>
2025-09-02 17:50:09 -0300

    Conditional render

--- renderizar-lista/src/components/ConditionalRender.jsx
+++ renderizar-lista/src/components/ConditionalRender.jsx
@@ -2,11 +2,24 @@ import { useState } from "react";
 
 const ConditionalRender = () => {
     const [x] = useState(false);
+    const [name,setName] = useState("Gabriel");
     return (
         <div>
             <h1>Isso será exibido ?</h1> 
             {x && <p>Se X for "true", sim!</p>}
             {!x && <p>Se X for "false", sim!</p>}
+            {name === "Gabriel" ? (
+                <div>
+                    <p>O nome é Gabriel</p>
+                </div>
+            ) : (
+                <div>
+                    <p>
+                        Nome não encontrado
+                    </p>
+                </div>
+            )}
+            <button onClick={() => setName("João")}>Clique aqui para trocar o nome</button>
         </div>
     );
 };
